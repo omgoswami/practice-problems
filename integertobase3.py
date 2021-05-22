da43e296fb6f275815f72a8544abae7851f73727
Om Goswami <62226812+omgoswami@users.noreply.github.com>
2021-05-22 00:28:13 -0700

    Update integertobase3.py

--- integertobase3.py
+++ integertobase3.py
@@ -1,7 +1,8 @@
 #take an integer as input and return the base 3 notation of that integer
-def solve(n):
-  s = ''
-  while n >= 3:
-    s = str(n%3) + s
-    n = n//3
-  return str(n)+s
+class Solution:
+  def solve(self,n):
+    s = ''
+    while n >= 3:
+      s = str(n%3) + s
+      n = n//3
+    return str(n)+s
